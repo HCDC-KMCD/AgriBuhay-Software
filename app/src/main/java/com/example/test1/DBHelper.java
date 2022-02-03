4196127ec2db1e2df10617728c636a6379352260
Jimson <jimson@davworkxasia.com>
2022-02-03 18:29:37 +0800

    First Test1

---
+++ app/src/main/java/com/example/test1/DBHelper.java
@@ -0,0 +1,57 @@
+package com.example.test1;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final String DBNAME = "Login.db";
+
+    public DBHelper(@Nullable Context context) {
+        super(context, "Login.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase DB) {
+        DB.execSQL("create Table users(username TEXT primary key, password TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase DB, int i, int i1) {
+        DB.execSQL("drop Table if exists users");
+    }
+
+    public Boolean insertData (String username, String password){
+        SQLiteDatabase DB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("username", username);
+        contentValues.put("password", password);
+        long result = DB.insert("users", null, contentValues);
+        if(result == -1)return false;
+        else
+            return true;
+    }
+
+    public Boolean checkUsername (String username){
+        SQLiteDatabase DB = this.getWritableDatabase();
+        Cursor cursor = DB.rawQuery("Select * from users where username = ?", new String[] {username});
+        if (cursor.getCount()>0)
+            return true;
+        else
+            return false;
+    }
+
+    public Boolean checkUsernamePassword (String username, String password){
+        SQLiteDatabase DB = this.getWritableDatabase();
+        Cursor cursor = DB.rawQuery("Select * from users where username = ? and password = ?", new String[]{username, password});
+        if(cursor.getCount()>0)
+            return true;
+        else
+            return false;
+    }
+}
